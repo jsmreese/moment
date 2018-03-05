52b1e414b5c36d7d13c5718bfeb78b6234ab811a
John Sharp Madhavan-Reese <john.madhavan-reese@youi.tv>
2018-03-04 22:46:54 -0500

    Improve symmetry of dayToMonth and monthToDay conversion.

--- src/lib/duration/bubble.js
+++ src/lib/duration/bubble.js
@@ -1,6 +1,9 @@
 import absFloor from '../utils/abs-floor';
 import absCeil from '../utils/abs-ceil';
+import absRound from '../utils/abs-round';
 import { createUTCDate } from '../create/date-from-array';
+
+var mathAbs = Math.abs;
 
 export function bubble () {
     var milliseconds = this._milliseconds;
@@ -49,13 +52,42 @@ export function bubble () {
     return this;
 }
 
+// Converting between Months and Days
+//
+// 400 years have 146097 days, taking into account leap year rules.
+// 400 years have 4800 months (12 * 400).
+// Each month has 30.436875 days on average (146097 / 4800), but any
+// single month has a whole number of days.
+//
+// To calculate the number of days for a given number of months,
+// multiply months by 30.436875 then make a modified round on the result.
+// AbsFloor the result, unless the result comes to within 12 hours of
+// the next whole month... then Round the result.
+// 12 hours in terms of the average month is 0.0164274420419588 months.
+//
+// Both calculations below are based on this logic, which is meant to
+// respect leap year effects on large-value durations and also to give
+// intuitive and symmetric conversions.
+//
+// These calculations are symmetric for converting whole months to days
+// and back again for all 4800 months in the 400-year cycle, and give
+// the correct value of exactly 146097 days for 4800 months.
+var AVERAGE_DAYS_PER_MONTH = 30.436875;
+var AVERAGE_MONTHS_PER_HALF_DAY = 0.0164274420419588;
+
 export function daysToMonths (days) {
-    // 400 years have 146097 days (taking into account leap year rules)
-    // 400 years have 12 months === 4800
-    return days * 4800 / 146097;
+    var fractionOfAverageMonth = days / AVERAGE_DAYS_PER_MONTH;
+    var roundedWholeMonths = absRound(fractionOfAverageMonth);
+    var absDifference = mathAbs(roundedWholeMonths - fractionOfAverageMonth);
+    var wholeMonths = absDifference > 0 && absDifference < AVERAGE_MONTHS_PER_HALF_DAY ? roundedWholeMonths : absFloor(fractionOfAverageMonth);
+
+    if (wholeMonths === 0) {
+        return fractionOfAverageMonth;
+    }
+
+    return wholeMonths + daysToMonths(days - monthsToDays(wholeMonths));
 }
 
 export function monthsToDays (months) {
-    // the reverse of daysToMonths
-    return months * 146097 / 4800;
+    return absRound(months * AVERAGE_DAYS_PER_MONTH);
 }
